39354f878181d5a24b7724e488bb76819d518e34
Baeron <baeron.a@outlook.com>
2017-12-05 14:20:08 +0900

    Added LUIS framework.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -24,7 +24,9 @@ namespace Microsoft.Bot.Sample.QnABot
             // check if activity is of type message
             if (activity.GetActivityType() == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new BasicQnAMakerDialog());
+                //await Conversation.SendAsync(activity, () => new BasicQnAMakerDialog());
+                await Conversation.SendAsync(activity, () => new RootDialog());
+
             }
             else
             {
---
+++ Dialogs/RootDialog.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Microsoft.Bot.Sample.QnABot
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    //using AdaptiveCards;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+    //using Util;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+
+    [Serializable]
+    [LuisModel("{a286e61f-c49c-4195-a053-3829dd492d7f}", "{5352577285224b61a75e8bcdfcff2141}"   )]
+    public class RootDialog : LuisDialog<object>
+    {
+        private string category;
+        private string severity;
+        private string description;
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"I'm sorry, I did not understand {result.Query}.\nType 'help' to know more about me :)");
+            context.Done<object>(null);
+        }
+
+        //[LuisIntent("Help")]
+        //public async Task Help(IDialogContext context, LuisResult result)
+        //{
+        //    await context.PostAsync("I'm the PS Helpbot and I can help you make a request, submit issues or answer your questions\n" +
+        //                            "You can tell me things like _I need to reset my password_ or _I am seeing an error_ or _How do I upgrade my membership_.");
+        //    context.Done<object>(null);
+    //}
+
+        [LuisIntent("RequestService")]
+        public async Task SubmitTicket(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation categoryEntityRecommendation, severityEntityRecommendation;
+
+            result.TryFindEntity("category", out categoryEntityRecommendation);
+            result.TryFindEntity("severity", out severityEntityRecommendation);
+
+            this.category = ((Newtonsoft.Json.Linq.JArray)categoryEntityRecommendation?.Resolution["values"])?[0]?.ToString();
+            this.severity = ((Newtonsoft.Json.Linq.JArray)severityEntityRecommendation?.Resolution["values"])?[0]?.ToString();
+            this.description = result.Query;
+
+            await this.EnsureInput(context);
+        }
+
+        private async Task EnsureInput(IDialogContext context)
+        {
+            if (this.severity == null)
+            {
+                var severities = new string[] { "high", "normal", "low" };
+                PromptDialog.Choice(context, this.SeverityMessageReceivedAsync, severities, "Which is the severity/urgency of this request/problem?");
+            }
+            else if (this.category == null)
+            {
+                PromptDialog.Text(context, this.CategoryMessageReceivedAsync, "Which would be the category for this (issue, request, query)?");
+            }
+            else
+            {
+                //For modification
+                var text = $"Great! I'm going to create a **{this.severity}** severity ticket in the **{this.category}** category. " +
+                        $"The description I will use is _\"{this.description}\"_. Can you please confirm that this information is correct?";
+
+                PromptDialog.Confirm(context, this.IssueConfirmedMessageReceivedAsync, text);
+            }
+        }
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("Hi! I’m the PS Helpbot and I will help you with your request");
+            PromptDialog.Text(context, this.DescriptionMessageReceivedAsync, "May I please know what you need today?");
+        }
+
+        public async Task DescriptionMessageReceivedAsync(IDialogContext context, IAwaitable<string> argument)
+        {
+            this.description = await argument;
+            var severities = new string[] { "high", "normal", "low" };
+            PromptDialog.Choice(context, this.SeverityMessageReceivedAsync, severities, "Which is the severity/urgency of this request?");
+        }
+
+        public async Task SeverityMessageReceivedAsync(IDialogContext context, IAwaitable<string> argument)
+        {
+            this.severity = await argument;
+            await this.EnsureInput(context);
+        }
+
+        public async Task CategoryMessageReceivedAsync(IDialogContext context, IAwaitable<string> argument)
+        {
+            this.category = await argument;
+            await this.EnsureInput(context);
+        }
+
+        public async Task IssueConfirmedMessageReceivedAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirmed = await argument;
+
+            if (confirmed)
+            {
+                //For modification
+                await context.PostAsync("Feature coming soon");
+            }
+            else
+            {
+                await context.PostAsync("Ok. The ticket was not created. You can start again if you want.");
+            }
+
+            context.Done<object>(null);
+        }
+
+        
+    }
+}
